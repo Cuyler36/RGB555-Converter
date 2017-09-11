2dd699302e0e9c26049fb219b4a27dc4da788522
Cuyler36 <jeremy96@vt.edu>
2017-09-11 04:52:41 -0400

    Add project files.

---
+++ RGB555 Color Picker/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RGB555_Color_Picker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rgb555Box = new System.Windows.Forms.TextBox();
+            this.hexBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.previewPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // rgb555Box
+            // 
+            this.rgb555Box.Location = new System.Drawing.Point(69, 6);
+            this.rgb555Box.MaxLength = 4;
+            this.rgb555Box.Name = "rgb555Box";
+            this.rgb555Box.Size = new System.Drawing.Size(55, 20);
+            this.rgb555Box.TabIndex = 0;
+            this.rgb555Box.Text = "0000";
+            // 
+            // hexBox
+            // 
+            this.hexBox.Location = new System.Drawing.Point(69, 32);
+            this.hexBox.MaxLength = 6;
+            this.hexBox.Name = "hexBox";
+            this.hexBox.Size = new System.Drawing.Size(55, 20);
+            this.hexBox.TabIndex = 1;
+            this.hexBox.Text = "000000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "RGB555:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hex:";
+            // 
+            // previewPanel
+            // 
+            this.previewPanel.BackColor = System.Drawing.Color.Black;
+            this.previewPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.previewPanel.Location = new System.Drawing.Point(130, 6);
+            this.previewPanel.Name = "previewPanel";
+            this.previewPanel.Size = new System.Drawing.Size(46, 46);
+            this.previewPanel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(188, 66);
+            this.Controls.Add(this.previewPanel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hexBox);
+            this.Controls.Add(this.rgb555Box);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RGB555 Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox rgb555Box;
+        private System.Windows.Forms.TextBox hexBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel previewPanel;
+    }
+}
+
---
+++ RGB555 Color Picker/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGB555_Color_Picker
+{
+    public partial class Form1 : Form
+    {
+        private string Previous_555_Text = "";
+        private string Previous_Hex_Text = "";
+        private bool Changing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rgb555Box.TextChanged += Rgb555Box_TextChanged;
+            hexBox.TextChanged += HexBox_TextChanged;
+        }
+
+        private void Rgb555Box_TextChanged(object sender, EventArgs e)
+        {
+            if (Changing)
+            {
+                return;
+            }
+
+            Changing = true;
+            rgb555Box.Text = rgb555Box.Text.ToUpper();
+            if (ushort.TryParse(rgb555Box.Text, NumberStyles.AllowHexSpecifier, null, out ushort RGB))
+            {
+                hexBox.Text = RGB555_to_Hex(RGB).ToString("X6");
+                Previous_555_Text = rgb555Box.Text;
+            }
+            else if (!string.IsNullOrEmpty(rgb555Box.Text))
+            {
+                rgb555Box.Text = Previous_555_Text;
+            }
+            else
+            {
+                Previous_555_Text = "";
+                hexBox.Text = "000000";
+            }
+            previewPanel.BackColor = Color.FromArgb((int)(0xFF000000 + uint.Parse(hexBox.Text, NumberStyles.AllowHexSpecifier, null)));
+            rgb555Box.SelectionStart = rgb555Box.Text.Length;
+            rgb555Box.SelectionLength = 0;
+            Changing = false;
+        }
+
+        private void HexBox_TextChanged(object sender, EventArgs e)
+        {
+            if (Changing)
+            {
+                return;
+            }
+
+            Changing = true;
+            hexBox.Text = hexBox.Text.ToUpper();
+            if (uint.TryParse(hexBox.Text, NumberStyles.AllowHexSpecifier, null, out uint Hex))
+            {
+                rgb555Box.Text = Hex_to_RGB555(Hex).ToString("X4");
+                Previous_Hex_Text = hexBox.Text;
+            }
+            else if (!string.IsNullOrEmpty(hexBox.Text))
+            {
+                hexBox.Text = Previous_Hex_Text;
+            }
+            else
+            {
+                Previous_Hex_Text = "";
+                rgb555Box.Text = "0000";
+            }
+            previewPanel.BackColor = Color.FromArgb((int)(0xFF000000 + uint.Parse(hexBox.Text, NumberStyles.AllowHexSpecifier, null)));
+            hexBox.SelectionStart = hexBox.Text.Length;
+            hexBox.SelectionLength = 0;
+            Changing = false;
+        }
+
+        // From: https://social.msdn.microsoft.com/Forums/vstudio/en-US/0fd7134a-0bd8-4460-9ed5-012e038d9054/16bpp-color-format-help?forum=csharpgeneral
+        private ushort Hex_to_RGB555(uint Hex)
+        {
+            uint R = (Hex >> 16) & 0xFF;
+            uint G = (Hex >> 8) & 0xFF;
+            uint B = Hex & 0xFF;
+            uint r = R * 31 / 255;
+            uint g = G * 31 / 255;
+            uint b = B * 31 / 255;
+
+            return (ushort)(r << (10) | (g << 5) | b);
+        }
+
+        private uint RGB555_to_Hex(ushort RGB)
+        {
+            uint r = (uint)((RGB >> (10)) & 31);
+            uint g = (uint)((RGB >> 5) & 31);
+            uint b = (uint)(RGB & 31);
+            uint R = r * 255 / 31;
+            uint G = g * 255 / 31;
+            uint B = b * 255 / 31;
+
+            return (R << 16) | (G << 8) | B;
+        }
+    }
+}
